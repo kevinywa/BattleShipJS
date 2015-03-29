6865e3afeae6eaae7d8780484577178dc92909ab
Alex Glenn-Murray <alex.glenn.murray@gmail.com>
2015-03-29 15:46:30 -0700

    added some functions

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ console.log(grid);
 
 var ships = [];
 
-var aircraftCarrier = {
+var aircraftcarrier = {
   symbol:"A",
   size:5,
   isHorizontal:false,
@@ -51,17 +51,16 @@ var destroyer = {
   symbol:"D",
   size:2,
   isHorizontal:false,
-  posX:3,
-  posY:5,
+  posX:0,
+  posY:0,
   hits:0
 };
 
-ships[ships.length] = aircraftCarrier;
+ships[ships.length] = aircraftcarrier;
 ships[ships.length] = battleship;
 ships[ships.length] = cruiser;
 ships[ships.length] = submarine;
 ships[ships.length] = destroyer;
-
 
 function writeGrid() {
   document.getElementById('board').innerHTML = grid[0];
@@ -76,24 +75,54 @@ function writeGrid() {
   document.getElementById('board').innerHTML += '<br>'+grid[9];
 };
 
-function addAircraftCarrier() {
-  for (i=0; i<=aircraftCarrier.size; i++) {
-    if (aircraftCarrier.isHorizontal === false) {
-      grid[aircraftCarrier.posX+i][aircraftCarrier.posY] = aircraftCarrier.symbol;
-    } else {grid[aircraftCarrier.posX][aircraftCarrier.posY+i] = aircraftCarrier.symbol};
+function addaircraftcarrier() {
+  for (i=0; i<=aircraftcarrier.size; i++) {
+    if (aircraftcarrier.isHorizontal === false) {
+      grid[aircraftcarrier.posX+i][aircraftcarrier.posY] = aircraftcarrier.symbol;
+    } else {grid[aircraftcarrier.posX][aircraftcarrier.posY+i] = aircraftcarrier.symbol};
   };
   writeGrid();
 };
-
-function randomizeAircraftCarrier() {
-  if (Math.floor((Math.random() * 10) + 1) <= 5) {
-    aircraftCarrier.isHorizontal = true
-  } else {aircraftCarrier.isHorizontal = false};
-  if (aircraftCarrier.isHorizontal === true) {
-    aircraftCarrier.posX = (Math.floor((Math.random() * 5)));
-    aircraftCarrier.posY = (Math.floor((Math.random() * 10)));
-  } else {
-    aircraftCarrier.posX = (Math.floor((Math.random() * 10)));
-    aircraftCarrier.posY = (Math.floor((Math.random() * 5)));
+function addbattleship() {
+  for (i=0; i<=battleship.size; i++) {
+    if (battleship.isHorizontal === false) {
+      grid[battleship.posX+i][battleship.posY] = battleship.symbol;
+    } else {grid[battleship.posX][battleship.posY+i] = battleship.symbol};
   };
+  writeGrid();
+};function addcruiser() {
+  for (i=0; i<=cruiser.size; i++) {
+    if (cruiser.isHorizontal === false) {
+      grid[cruiser.posX+i][cruiser.posY] = cruiser.symbol;
+    } else {grid[cruiser.posX][cruiser.posY+i] = cruiser.symbol};
+  };
+  writeGrid();
+};function addsubmarine() {
+  for (i=0; i<=submarine.size; i++) {
+    if (submarine.isHorizontal === false) {
+      grid[submarine.posX+i][submarine.posY] = submarine.symbol;
+    } else {grid[submarine.posX][submarine.posY+i] = submarine.symbol};
+  };
+  writeGrid();
+};function adddestroyer() {
+  for (i=0; i<=destroyer.size; i++) {
+    if (destroyer.isHorizontal === false) {
+      grid[destroyer.posX+i][destroyer.posY] = destroyer.symbol;
+    } else {grid[destroyer.posX][destroyer.posY+i] = destroyer.symbol};
+  };
+  writeGrid();
+};
+function randomizeShips() {
+  if (Math.floor((Math.random() * 10) + 1) <= 5) {
+    aircraftcarrier.isHorizontal = true
+  } else {aircraftcarrier.isHorizontal = false};
+  if (aircraftcarrier.isHorizontal === true) {
+    aircraftcarrier.posX = (Math.floor((Math.random() * 4)));
+    aircraftcarrier.posY = (Math.floor((Math.random() * 9)));
+  } else {
+    aircraftcarrier.posX = (Math.floor((Math.random() * 9)));
+    aircraftcarrier.posY = (Math.floor((Math.random() * 4)));
+  };
+  addaircraftcarrier();
+  if (aircraftcarrier.isHorizontal === true and )//TODO
 };
