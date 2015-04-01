d16c96bf873e7cd67d569e669e29e60a4c986f01
Alex Glenn-Murray <alex.glenn.murray@gmail.com>
2015-03-31 18:50:47 -0700

    shuffle ships and add the first two to the grid

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ function addaircraftcarrier() {
       grid[aircraftcarrier.posX+i][aircraftcarrier.posY] = aircraftcarrier.symbol;
     } else {grid[aircraftcarrier.posX][aircraftcarrier.posY+i] = aircraftcarrier.symbol};
   };
-  writeGrid();
+
 };
 function addbattleship() {
   for (i=0; i<=battleship.size; i++) {
@@ -89,7 +89,7 @@ function addbattleship() {
       grid[battleship.posX+i][battleship.posY] = battleship.symbol;
     } else {grid[battleship.posX][battleship.posY+i] = battleship.symbol};
   };
-  writeGrid();
+
 };
 function addcruiser() {
   for (i=0; i<=cruiser.size; i++) {
@@ -97,7 +97,7 @@ function addcruiser() {
       grid[cruiser.posX+i][cruiser.posY] = cruiser.symbol;
     } else {grid[cruiser.posX][cruiser.posY+i] = cruiser.symbol};
   };
-  writeGrid();
+
 };
 function addsubmarine() {
   for (i=0; i<=submarine.size; i++) {
@@ -105,7 +105,7 @@ function addsubmarine() {
       grid[submarine.posX+i][submarine.posY] = submarine.symbol;
     } else {grid[submarine.posX][submarine.posY+i] = submarine.symbol};
   };
-  writeGrid();
+
 };
 function adddestroyer() {
   for (i=0; i<=destroyer.size; i++) {
@@ -113,27 +113,103 @@ function adddestroyer() {
       grid[destroyer.posX+i][destroyer.posY] = destroyer.symbol;
     } else {grid[destroyer.posX][destroyer.posY+i] = destroyer.symbol};
   };
-  writeGrid();
-};
-function randomizeShips() {
-  if (Math.floor((Math.random() * 10) + 1) <= 5) {
-    aircraftcarrier.isHorizontal = true
-  } else {aircraftcarrier.isHorizontal = false};
-  if (aircraftcarrier.isHorizontal === true) {
-    aircraftcarrier.posX = (Math.floor((Math.random() * 4)));
-    aircraftcarrier.posY = (Math.floor((Math.random() * 9)));
-  } else {
-    aircraftcarrier.posX = (Math.floor((Math.random() * 9)));
-    aircraftcarrier.posY = (Math.floor((Math.random() * 4)));
-  };
-  addaircraftcarrier();
-    if (Math.floor((Math.random() * 10) + 1) <= 5) {
-      cruiser.isHorizontal = true
-    } else {
-      cruiser.isHorizontal = false
-    };
-    for (i=0; i <= 10 ; i++) {
 
-    }
-  }
 };
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+function addShips() {
+  addaircraftcarrier();
+  addbattleship();
+  addcruiser();
+  addsubmarine();
+  adddestroyer();
+};
+
+function randomPos() {
+  if (Math.random()<.5) {
+    ships[0].isHorizontal = true;
+  } else {
+    ships[0].isHorizontal = false;
+  };
+  if (Math.random()<.5) {
+    ships[1].isHorizontal = true;
+  } else {
+    ships[1].isHorizontal = false;
+  };
+  if (Math.random()<.5) {
+    ships[2].isHorizontal = true;
+  } else {
+    ships[2].isHorizontal = false;
+  };
+  if (Math.random()<.5) {
+    ships[3].isHorizontal = true;
+  } else {
+    ships[3].isHorizontal = false;
+  };
+  if (Math.random()<.5) {
+    ships[4].isHorizontal = true;
+  } else {
+    ships[4].isHorizontal = false;
+  };
+
+  if (ships[0].isHorizontal === true) {
+    ships[0].posX = getRandomInt(0, (9-ships[0].size));
+    ships[0].posY = getRandomInt(0,1);
+    //ships[1] nested if/else statement
+    if (ships[1].isHorizontal === true) {
+      //both [0] and [1] horizontal
+      ships[1].posX = getRandomInt(0,(9-ships[1].size));
+      ships[1].posY = (ships[0].posY+getRandomInt(1,2));
+    } else {
+      //[0] horizontal [1] vertical
+      ships[1].posX = (ships[0].posX+getRandomInt(1,2));
+      ships[1].posY = (getRandomInt((ships[0].posY+1),(9-ships[1].size)));
+    };
+  } else {
+    //ships[0].isHorizontal === false
+    ships[0].posX = getRandomInt(0,1);
+    ships[0].posY = getRandomInt(0, (9-ships[0].size));
+    //ships[1] nested if/else statement
+    if (ships[1].isHorizontal === true) {
+      //if ships[0].isHorizontal === false AND ships[1].isHorizontal === true THEN...
+      // checking if there's room for ships[1] on the x axis beside ships[0]
+      if (ships[0].posX < (9-ships[1].size)) {
+        ships[1].posX = (ships[0].posX+(getRandomInt(1,(9-ships[1].size))));
+        ships[1].posY = getRandomInt(0,1);
+      } else {
+        // [1] is horizontal and [0] is vertical
+        // [0].posX is greater than (grid[0].length - ships[1].size)
+        ships[1].posX = getRandomInt(0,(ships[0].posX-ships[1].size));
+        ships[1].posY = getRandomInt(2, 3)
+      };
+    } else {
+      //if ships[0].isHorizontal === false AND ships[1].isHorizontal === false THEN
+      ships[1].posX = (ships[0].posX+getRandomInt(1,2));
+      ships[1].posY = (getRandomInt((ships[0].posY+1),(9-ships[1].size)));
+    };
+  };
+};
+
+shuffle(ships);
+randomPos();
+addShips();
+console.log(grid);
